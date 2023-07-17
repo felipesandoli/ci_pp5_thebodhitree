a12cbb9e732f9b67b7cb56aaf2f752c992ba8566
felipesandoli <cfelipesandoli@gmail.com>
2023-07-17 01:25:52 +0100

    Add book now button

--- property/views.py
+++ property/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Property
 
 
 def property_details(request, id):
     """Displays property full details"""
+    property = get_object_or_404(Property, pk=id)
     template = 'property/property_details.html'
-    return render(request, template)
+    context = {
+        'property': property
+    }
+    return render(request, template, context)
