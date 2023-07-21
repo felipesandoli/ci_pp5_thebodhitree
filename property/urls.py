2213cd6ebec72a28578b247716c865f90171e3e1
felipesandoli <cfelipesandoli@gmail.com>
2023-07-21 21:02:37 +0100

    Create page for displaying all properties

--- property/urls.py
+++ property/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:property_id>/', views.property_details, name='property_details')
+    path('<int:property_id>/', views.property_details, name='property_details'),
+    path('all_properties/', views.all_properties, name='all_properties'),
 ]
--- property/views.py
+++ property/views.py
@@ -12,3 +12,13 @@ def property_details(request, property_id):
         'amenities': amenities
     }
     return render(request, template, context)
+
+
+def all_properties(request):
+    """Displays all available properties"""
+    properties = Property.objects.filter(is_available=True)
+    template = 'property/all_properties.html'
+    context = {
+        'properties': properties,
+    }
+    return render(request, template, context)
